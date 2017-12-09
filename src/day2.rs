14958ce98f2157bdad67b8def8821e666ffcd4ec
Samuel Laurén <samuel.lauren@iki.fi>
2017-12-09 22:36:25 +0200

    Day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,18 +1,38 @@
+extern crate itertools;
+use itertools::Itertools;
+
 const INPUT: &str = include_str!("input/day2.txt");
 
 fn main() {
-    let checksum: i32 = INPUT
+    let table: Vec<Vec<i32>> = INPUT
         .lines()
         .map(|s| {
-            let digits: Vec<i32> = s.trim()
+            s.trim()
                 .split('\t')
                 .map(|f| f
                      .parse()
                      .unwrap())
-                .collect();
-            let max = digits.iter().max().unwrap();
-            let min = digits.iter().min().unwrap();
+                .collect()
+        }).collect();
+
+    let checksum1: i32 = table
+        .iter()
+        .map(|r| {
+            let max = r.iter().max().unwrap();
+            let min = r.iter().min().unwrap();
             max - min
         }).sum();
-    println!("{}", checksum);
+
+    let checksum2: i32 = table
+        .iter()
+        .map(|r| r.iter()
+             .tuple_combinations()
+             .flat_map(|(a, b)| vec![(a, b), (b, a)])
+             .find(|&(a, b)| a % b == 0)
+             .map(|(a, b)| a / b)
+             .unwrap())
+        .sum();
+
+    println!("{}", checksum1);
+    println!("{}", checksum2);
 }
