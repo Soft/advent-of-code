7cfe0451377c213e09dbcd6660dbaf2a4bfc06f2
Samuel Laurén <samuel.lauren@iki.fi>
2017-12-09 23:11:25 +0200

    Day 4 Part 2

--- src/day4.rs
+++ src/day4.rs
@@ -1,16 +1,38 @@
 extern crate itertools;
 use itertools::Itertools;
 
+use std::collections::HashSet;
+use std::iter::FromIterator;
+
 const INPUT: &str = include_str!("input/day4.txt");
 
 fn main() {
-    let count = INPUT
+    let rows: Vec<Vec<_>> = INPUT
         .lines()
-        .filter(|s| {
-            let words: Vec<_> = s.split(' ').collect();
+        .map(|s| s.split(' ').collect())
+        .collect();
+
+    let count1 = rows
+        .iter()
+        .filter(|words| {
             words.iter().unique().count() == words.len()
         })
         .count();
-    println!("{}", count);
+
+    let count2 = rows
+        .iter()
+        .filter(|words| {
+            let sets: Vec<HashSet<_>> = words
+                .iter()
+                .map(|w| HashSet::from_iter(w.chars()))
+                .collect();
+            sets.iter()
+                .tuple_combinations()
+                .all(|(a, b)| a != b)
+        })
+        .count();
+
+    println!("{}", count1);
+    println!("{}", count2);
 }
 
