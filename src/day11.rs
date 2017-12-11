56fbaa450a3c97277494bae08716123b09cbe2cf
Samuel Laurén <samuel.lauren@iki.fi>
2017-12-11 14:57:55 +0200

    Day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -13,13 +13,13 @@ impl FromStr for Direction {
     type Err = ();
     fn from_str(s: &str) -> Result<Direction, ()> {
         match s {
-            "n" => Ok(Direction::N),
+            "n"  => Ok(Direction::N),
             "ne" => Ok(Direction::NE),
             "se" => Ok(Direction::SE),
-            "s" => Ok(Direction::S),
+            "s"  => Ok(Direction::S),
             "sw" => Ok(Direction::SW),
             "nw" => Ok(Direction::NW),
-            _ => Err(())
+            _    => Err(())
         }
     }
 }
@@ -29,22 +29,25 @@ fn distance((y1, x1, z1): Coords, (y2, x2, z2): Coords) -> i32 {
 }
 
 fn main() {
-    let coords = INPUT
+    let (coords, max) = INPUT
         .trim()
         .split(',')
         .map(|s| s.parse().unwrap())
-        .fold((0, 0, 0), |(y, x, z), d| {
-            match d {
+        .fold(((0, 0, 0), i32::min_value()), |((y, x, z), max), d| {
+            let coords = match d {
                 Direction::N  => (y+1, x,   z-1),
                 Direction::NE => (y,   x+1, z-1),
                 Direction::SE => (y-1, x+1, z),
                 Direction::S  => (y-1, x,   z+1),
                 Direction::SW => (y,   x-1, z+1),
                 Direction::NW => (y+1, x-1, z)
-            }
+            };
+            let dist = distance((0, 0, 0), coords);
+            let max = if dist > max { dist } else { max };
+            (coords, max)
         });
     println!("{}", distance((0, 0, 0), coords));
-
+    println!("{}", max);
 }
 
 
