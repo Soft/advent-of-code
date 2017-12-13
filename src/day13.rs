e3c768848d4e7c109b5c662b45160d8e96abb1e3
Samuel Laurén <samuel.lauren@iki.fi>
2017-12-13 20:23:47 +0200

    Day 13 part 2

--- src/day13.rs
+++ src/day13.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 const INPUT: &str = include_str!("input/day13.txt");
 
+#[derive(Clone, Debug)]
 struct Scanner {
     i: i32,
     range: i32,
@@ -20,18 +21,12 @@ impl Scanner {
     fn range(&self) -> i32 {
         self.range
     }
-}
 
-impl Iterator for Scanner {
-    type Item = i32;
-
-    fn next(&mut self) -> Option<i32> {
-        let v = self.i;
+    fn step(&mut self) {
         self.i += self.offset;
-        if self.i == self.range - 1 || self.i == 0 {
+        if self.i == self.range - 1 || (self.i == 0 && self.offset < 0) {
             self.offset *= -1;
         }
-        Some(v)
     }
 }
 
@@ -46,23 +41,47 @@ fn parse_input(input: &str) -> HashMap<usize, Scanner> {
     scanners
 }
 
+fn calculate_severity(layers: usize, mut scanners: &mut HashMap<usize, Scanner>) -> Option<i32> {
+    let mut severity = None;
+    for i in 0..layers+1 {
+        if let Some(current) = scanners.get(&i) {
+            if current.position() == 0 {
+                let severity = severity.get_or_insert(0);
+                *severity += i as i32 * current.range();
+            }
+        }
+        step_scanners(&mut scanners);
+    }
+    severity
+}
+
+fn step_scanners(scanners: &mut HashMap<usize, Scanner>) {
+    for scanner in scanners.values_mut() {
+        scanner.step();
+    }
+}
+
 fn main() {
-    let mut scanners = parse_input(INPUT);
+    let scanners = parse_input(INPUT);
     let layers = scanners.keys()
         .max()
         .unwrap()
         .to_owned();
-    let mut severity = 0;
 
-    for i in 0..layers+1 {
-        if let Some(current) = scanners.get(&i) {
-            if current.position() == 0 {
-                severity += i as i32 * current.range();
-            }
-        }
-        scanners.values_mut().for_each(|s| { s.next(); });
-    }
+    let mut scanners1 = scanners.clone();
+
+    let severity = calculate_severity(layers, &mut scanners1)
+        .unwrap_or(0);
 
     println!("{}", severity);
+
+    let mut scanners2 = scanners.clone();
+
+    let wait = (1..).find(|_| {
+        step_scanners(&mut scanners2);
+        calculate_severity(layers, &mut scanners2.clone()).is_none()
+    }).unwrap();
+
+    println!("{}", wait);
 }
 
