1415123705bff327539938c127b6068333d4c847
Samuel Laurén <samuel.lauren@iki.fi>
2017-12-15 11:13:35 +0200

    Day 15 part 2

--- src/day15.rs
+++ src/day15.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 const INPUT: (u64, u64) = (277, 349);
 
 fn main() {
@@ -8,6 +10,7 @@ fn main() {
     let mask = u16::max_value() as u64;
     let mut prev_a = INPUT.0;
     let mut prev_b = INPUT.1;
+
     for _ in 0..40_000_000 {
         prev_a = prev_a * mult_a % divider;
         prev_b = prev_b * mult_b % divider;
@@ -15,6 +18,36 @@ fn main() {
             count += 1;
         }
     }
+
+    println!("{}", count);
+
+    let mut a_stack = VecDeque::new();
+    let mut b_stack = VecDeque::new();
+    let mut comparisons = 0;
+
+    prev_a = INPUT.0;
+    prev_b = INPUT.1;
+    count = 0;
+
+    while comparisons < 5_000_000 {
+        prev_a = prev_a * mult_a % divider;
+        prev_b = prev_b * mult_b % divider;
+        if prev_a % 4 == 0 {
+            a_stack.push_back(prev_a);
+        }
+        if prev_b % 8 == 0 {
+            b_stack.push_back(prev_b);
+        }
+        if !a_stack.is_empty() && !b_stack.is_empty() {
+            let a_val = a_stack.pop_front().unwrap();
+            let b_val = b_stack.pop_front().unwrap();
+            comparisons += 1;
+            if a_val & mask == b_val & mask {
+                count += 1;
+            }
+        }
+    }
+
     println!("{}", count);
 }
 
