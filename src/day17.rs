2a8b449413d65368aeac061581f3c1612763b872
Samuel Laurén <samuel.lauren@iki.fi>
2017-12-18 22:26:46 +0200

    Day 17 part 2

--- src/day17.rs
+++ src/day17.rs
@@ -6,12 +6,29 @@ fn main() {
     buffer.push(0);
 
     for n in 1..2018 {
-        ind = (ind + INPUT + 1) % buffer.len();
+        ind = (ind + INPUT) % buffer.len() + 1;
         buffer.insert(ind, n);
     }
 
     let last = (ind + 1) % buffer.len();
+
     println!("{}", buffer[last]);
-    
+
+    ind = 0;
+    let mut zero_ind = 0;
+    let mut result = None;
+    let mut len = 1;
+
+    for n in 1..50_000_000+1 {
+        ind = ((ind + INPUT) % len) + 1;
+        if ind <= zero_ind {
+            zero_ind += 1;
+        } else if ind == (zero_ind+1) % len {
+            result = Some(n);
+        }
+        len += 1;
+    }
+
+    println!("{}", result.unwrap());
 }
 
