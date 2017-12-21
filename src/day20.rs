bbc67ca4411e19a17ad72ecd3112b9498b10aa95
Samuel Laurén <samuel.lauren@iki.fi>
2017-12-21 12:57:49 +0200

    Day 20

--- src/day20.rs
+++ src/day20.rs
@@ -1,7 +1,9 @@
 #[macro_use]
 extern crate nom;
+extern crate itertools;
 
 use nom::digit;
+use itertools::Itertools;
 
 const INPUT: &str = include_str!("input/day20.txt");
 
@@ -29,14 +31,31 @@ fn distance((x1, y1, z1): Triplet, (x2, y2, z2): Triplet) -> i64 {
     (x1 - x2).abs() + (y1 - y2).abs() + (z1 - z2).abs()
 }
 
+fn grouped_min_by<I, E, F, K>(iter: I, key: F) -> Vec<E>
+    where I: Iterator<Item=E>,
+          F: FnMut(&E) -> K,
+          K: PartialEq + Ord + Clone {
+    let grouped = iter.group_by(key);
+    let grouped = grouped.into_iter()
+        .min_by_key(|&(ref k, _)| k.clone())
+        .map(|v| v.1.collect::<Vec<_>>())
+        .unwrap_or_else(|| vec![]);
+    grouped
+}
+
 fn main() {
     let input = parse_input(INPUT).unwrap().1;
+    let input: Vec<_> = input.into_iter().enumerate().collect();
 
-    let min = input.iter()
-        .enumerate()
-        .min_by_key(|&(_, &(_, _, v))| distance((0,0,0), v));
+    let mut min = grouped_min_by(input.into_iter(), |&(_, (_, _, a))| distance((0, 0, 0), a));
+    if min.len() > 1 {
+        min = grouped_min_by(min.into_iter(), |&(_, (_, v, _))| distance((0, 0, 0), v));
+        if min.len() > 1 {
+            min = grouped_min_by(min.into_iter(), |&(_, (p, _, _))| distance((0, 0, 0), p));
+        }
+    }
 
-    println!("{}", min.unwrap().0);
+    println!("{}", min[0].0);
 }
 
 
